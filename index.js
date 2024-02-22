0d14297ef4c12b4f71be64b34d441ce7b12cf64e
deepesh798G <deepeshahirwar7024@gmail.com>
2024-02-22 10:08:38 +0530

    version2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function refresh() {
     document.querySelector('body').style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .3)), url(' + imageUrl + ')';
 }  
 //the function will run in every 5 seconds
-setInterval(refresh,5000);  
+ setInterval(refresh,90000);  
  
 
 
@@ -19,14 +19,16 @@ const quoteText = document.querySelector(".quote"),
   authorName = document.querySelector(".name"), 
    soundBtn =  document.querySelector(".sound"), 
    copyBtn =  document.querySelector(".copy"),
-   twitterBtn =  document.querySelector(".twitter");
+   twitterBtn =  document.querySelector(".twitter"); 
+
+ 
  // random quote function 
  function randomQuote(){ 
     quoteBtn.innerHTML="Loading...";
   fetch("https://api.quotable.io/random").then(res => res.json()) .then(result =>{
     console.log(result); 
     //author , content 
-    quoteText.innerHTML = result.content; 
+    quoteText.innerHTML = `" ${result.content} "`; 
     authorName.innerHTML = result.author; 
     quoteBtn.innerHTML="New Quote";
   }); 
